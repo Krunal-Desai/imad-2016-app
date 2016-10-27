18305ee48e5542ef6074635031641ea624c2424c
Abhi1507 <ahgandhi95@gmail.com>
2016-10-27 12:38:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,77 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+
+button.onclick=function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.status === 200) {
+                var counter = request.responseText;
+                
+                var span = document.getElementById('count');
+    
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+    
+    request.open('GET','http://krunal-desai.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+
+};
+
+//Submit name
+
+
+
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function()
+{
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() 
+    {
+        
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            
+            if(request.status === 200) 
+            {
+                var names = request.responseText;
+                
+                names=JSON.parse(names);
+                
+                var list = '';
+
+                for(var i=0 ; i<names.length;i++)
+                    {
+                        list = list + '<li>' + names[i] + '</li>';
+                    }
+
+                var ul = document.getElementById('namelist');
+                
+                ul.innerHTML = list;
+            }
+        }
+        
+    };
+    
+var nameInput = document.getElementById('name');
+
+var game = nameInput.value;
+    
+
+request.open('GET','http://krunal-desai.imad.hasura-app.io/submit-name?name=' + game,true);
+request.send(null);
+
+};
+
+
+    
