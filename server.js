309716c4689b8b8ec573c9ea7bc5f8a693f13b16
Krunal-Desai <desaik231@gmail.com>
2016-10-29 04:11:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+//Database set up
+var Pool = require('pg').Pool;
+
+var config = {
+  
+  user: 'krunal-desai',
+  database: 'krunal-desai',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -22,6 +34,32 @@ app.get('/ui/img/:articleName', function (req, res) {
     var data = req.params.articleName;
     
     res.sendFile(path.join(__dirname, 'ui','img',data));
+});
+
+var pool = new Pool(config);
+
+app.get('ui/img/:imgname', function (req, res) {
+   
+   pool.query("SELECT * FROM imagedata WHERE imagename = '$1'",[req.params.imgname],function(err,result){
+       
+        if(err)
+      {
+                res.status(500).send(err.toString());
+      }
+      
+      else
+      {
+                if(result.rows.length === 0)
+                    res.status(404).send('imgNotFound');
+                    
+                else
+                    {
+                        var imgNameData = result.rows[0];
+                        
+                        res.sendFile(path.join(__dirname,'ui','img', imgNameData));
+                    }
+      }
+   });
 });
 
 
