b847408f72005bc99b8e32d038c0f0f97bb36629
Krunal-Desai <desaik231@gmail.com>
2016-10-29 00:13:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,14 +13,6 @@ app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/swipebox.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'swipebox.css'));
-});
-
-app.get('/bootstrap.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
