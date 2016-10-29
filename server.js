423ce494af5e3d3c43fafcafa7bca2f32ac61409
Krunal-Desai <desaik231@gmail.com>
2016-10-29 06:10:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,6 +34,14 @@ app.get('/assets/js/:js', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','assets','js',jsdata));
 });
 
+//Font Servent
+app.get('/assets/font/:font', function (req, res) {
+
+  var fontdata = req.params.font;
+
+  res.sendFile(path.join(__dirname,'ui','assets','font',fontdata));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
